3781c8ce9fa460361610f947096d8fb88fa02ff9
daveeedu <daveeedu@gmail.com>
2022-05-10 21:43:29 +0100

    add updates

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { HashRouter}  from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter
-  basename='/link-shortener'
 >
   <App />
 </HashRouter>
