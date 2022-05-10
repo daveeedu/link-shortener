4efc066f3c76e3147237bfcf92dfcaf85bc7310f
daveeedu <daveeedu@gmail.com>
2022-05-10 21:59:57 +0100

    add updates

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <HashRouter >
         <Header />
         <Routes>
-          <Route exact path="/HomePage" element={<HomePage />}/>
+          <Route exact path="/" element={<HomePage />}/>
           <Route path="/AboutPage" element={<AboutPage />}/>
           <Route path="/ContactPage" element={<ContactPage />}/>
           </Routes>
